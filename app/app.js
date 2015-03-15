f40014434a3631f680f00224b1dda79eb7de7165
Nick Pisani <napisani@yahoo.com>
2015-03-14 21:41:31 -0400

    initial commit - still just testing gpio library

---
+++ app/app.js
@@ -0,0 +1,11 @@
+'use strict';
+/**
+ * Created by napisani on 3/14/15.
+ */
+var gpio = require("pi-gpio");
+
+gpio.open(16, "output", function(err) {     // Open pin 16 for output
+    gpio.write(16, 1, function() {          // Set pin 16 high (1)
+        gpio.close(16);                     // Close pin 16
+    });
+});
