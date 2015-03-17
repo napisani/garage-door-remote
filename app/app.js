4312d553b8274add36f43f9b0cb5e06136ccfe89
Nick Pisani <napisani@yahoo.com>
2015-03-16 23:36:18 -0400

    got the pins correct - and got the relay switches to operate. This code still needs to be hooked into the web, cleaned up, etc.

--- app/app.js
+++ app/app.js
@@ -2,10 +2,29 @@
 /**
  * Created by napisani on 3/14/15.
  */
-var gpio = require("pi-gpio");
+var gpio = require('rpi-gpio');
+var inputToGPIO = [11, 15, 18, 22];
+var delay = 2000;
 
-gpio.open(16, "output", function(err) {     // Open pin 16 for output
-    gpio.write(16, 1, function() {          // Set pin 16 high (1)
-        gpio.close(16);                     // Close pin 16
+var writeToPin = function (pin, value) {
+    gpio.write(pin, value, function (err) {
+        console.log("Working on pin: " + pin);
+        if (err) {
+            throw err;
+        }
+        console.log('Wrote value: ' + value + ' to pin: ' + pin);
     });
-});
+};
+
+var setupPins = function (pin) {;
+    console.log('Setting up GPIO pin:' + pin );
+    gpio.setup(pin, gpio.DIR_OUT, function(){
+        writeToPin(pin, false);
+    });
+};
+for(var index = 0; index < inputToGPIO.length; index++) {
+    setupPins(inputToGPIO[index]);
+}
+//for (var index = 0; index < inputToGPIO.length; index++) {
+//    writeToPin(inputToGPIO[index], false);
+//}
